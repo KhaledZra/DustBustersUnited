1015f2ab70be69050a1dbaa3a49f66ab001793d6
Omar <omar.m10@hotmail.com>
2023-10-11 12:41:06 +0200

    still in progress

--- Client/App.tsx
+++ Client/App.tsx
@@ -1,19 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import ThemeProvider from "./src/themes/ThemeContext";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaProvider>
       <StatusBar style="auto" />
-    </View>
+      <ThemeProvider>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
---
+++ Client/src/themes/ThemeContext.tsx
@@ -0,0 +1,39 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { PropsWithChildren, createContext, useContext, useState } from "react";
+import { useColorScheme } from "react-native";
+import { PaperProvider } from "react-native-paper";
+
+import { AppDarkTheme, AppLightTheme } from "./appThemes";
+
+type ColorScheme = "light" | "dark" | "auto";
+
+type ThemeContextValue = {
+  setColorScheme: (scheme: ColorScheme) => void;
+};
+
+const ThemeContext = createContext<ThemeContextValue>({} as ThemeContextValue);
+
+export default function ThemeProvider({ children }: PropsWithChildren) {
+  //Temat som användaren valt i appen
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("dark");
+
+  //Temat som OS'et föreslår
+  const autoScheme = useColorScheme();
+
+  const selectedScheme = colorScheme === "auto" ? autoScheme : colorScheme;
+
+  //välj rätt teamobjekt utifrån valt tema
+  const theme = selectedScheme === "dark" ? AppDarkTheme : AppLightTheme;
+
+  console.log(theme);
+
+  return (
+    <ThemeContext.Provider value={{ setColorScheme }}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer theme={theme}>{children}</NavigationContainer>
+      </PaperProvider>
+    </ThemeContext.Provider>
+  );
+}
+
+export const useSetColorTheme = () => useContext(ThemeContext);
