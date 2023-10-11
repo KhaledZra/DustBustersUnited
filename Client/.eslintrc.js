3c726088a19135e68efb6216b99570ac38d4d3c8
MarcusSakae <82marsak@skola.boras.se>
2023-10-11 19:42:53 +0200

    CLRF rules were strange, prob because of setting up on OSX which uses CL

--- Client/.eslintrc.js
+++ Client/.eslintrc.js
@@ -1,7 +1,32 @@
 module.exports = {
-    root: true,
-    extends: [
-      'universe/native',
-    ],
-  };
-  
+  env: {
+    browser: true,
+    es2021: true,
+    "react-native/react-native": true,
+  },
+  extends: ["universe/native", "plugin:react/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    jsx: true,
+  },
+  plugins: ["@typescript-eslint", "react", "react-native"],
+  rules: {
+    quotes: ["warn", "double"],
+    "react/react-in-jsx-scope": "off",
+    semi: ["warn", "always"],
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
+};
