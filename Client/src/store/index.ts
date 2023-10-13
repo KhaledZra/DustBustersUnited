75635e16bd4e37be00d10dd40010d1696dc1e9e4
MarcusSakae <82marsak@skola.boras.se>
2023-10-13 11:36:35 +0200

    * removed combineReducers * added typed shortcut to useSelector

--- Client/src/store/index.ts
+++ Client/src/store/index.ts
@@ -1,15 +1,16 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 import userSlice from "./userSlice";
 
-export const rootReducer = combineReducers({
-  user: userSlice,
+const store = configureStore({
+  reducer: {
+    user: userSlice,
+  },
 });
-
-const store = configureStore({ reducer: rootReducer });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export default store;
