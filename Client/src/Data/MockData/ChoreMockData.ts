2052fe280716c4085628f3d29438b44b31b80468
Khaled Zra <halo_434000@hotmail.com>
2023-10-19 12:27:56 +0200

    Mock data are now connected and use each other

--- Client/src/Data/MockData/ChoreMockData.ts
+++ Client/src/Data/MockData/ChoreMockData.ts
@@ -1,18 +1,5 @@
 import { Chore } from "../Chore";
-import { Household } from "../Household";
-import { User } from "../User";
-
-const mockUser: User = {
-  id: 1,
-  userName: "Marcus Sake",
-  password: "Japan",
-};
-const mockHousehold: Household = {
-  id: 1,
-  name: "Marcus Sake",
-  code: 1234,
-  owner: mockUser,
-};
+import { mockHousehold } from "./HouseHoldMockData";
 
 export const mockChores: Chore[] = [
   {
