4f4175160f71cfc35faf1e49ab6ff07fbd37661d
Khaled Zra <halo_434000@hotmail.com>
2023-10-23 12:23:17 +0200

    AvatarPicker refactored + added new styles

--- Client/src/utils/globalStyles.ts
+++ Client/src/utils/globalStyles.ts
@@ -1,15 +1,31 @@
 import { StyleSheet } from "react-native";
 
-export const globalStyle = StyleSheet.create({
+const globalStyle = StyleSheet.create({
+  // Flex styles
   flex: { display: "flex" },
   flex1: { flex: 1 },
-  radiusNone: { borderRadius: 0 },
   row: { flexDirection: "row" },
+  wrap: { flexWrap: "wrap" },
+
+  // Placement Styles
   justifyBetween: { justifyContent: "space-between" },
+  justifyCenter: { justifyContent: "center" },
   alignCenter: { alignItems: "center" },
+
+  // Border Styles
+  
+  radiusNone: { borderRadius: 0 },
+  br4: { borderRadius: 4 },
+  bw2: {borderWidth: 2},
+  bcTransparent: {borderColor: "transparent"},
+
+  // Gap
   gap1: { gap: 1 },
   gap2: { gap: 2 },
   gap4: { gap: 4 },
+  gap10: { gap: 10 },
+
+  // Padding
   p1: { padding: 1 },
   p2: { padding: 2 },
   p4: { padding: 4 },
@@ -18,9 +34,29 @@ export const globalStyle = StyleSheet.create({
   p12: { padding: 12 },
   p16: { padding: 16 },
   p20: { padding: 20 },
+
+  // Padding Horizontal
   ph1: { paddingHorizontal: 1 },
   ph2: { paddingHorizontal: 2 },
   pv1: { paddingVertical: 1 },
   pv2: { paddingVertical: 2 },
+
+  // Margin Vertical
+  mv10: {marginVertical: 10},
+
+  // Width
+  w20: { width: '20%'},
+  w40: { width: '40%'},
+
+  // Aspect Ratio
+  ar1: { aspectRatio: 1},
+
+  // Elevation
+  e4: {elevation: 4},
+
+  // Text Styles
   boldText: { fontWeight: "bold" },
 });
+
+
+export default globalStyle;
