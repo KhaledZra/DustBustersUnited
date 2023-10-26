9fcc56c3ee26336ba1e82a8d708fee8631e7b631
Mohammed7m <113667097+Mohammed7m@users.noreply.github.com>
2023-10-26 10:30:39 +0200

    added and styled profileScreen

--- Client/src/Screens/ProfileScreen.tsx
+++ Client/src/Screens/ProfileScreen.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { Profiler } from "react";
+import { StyleSheet, View } from "react-native";
 import { Button } from "react-native-paper";
 import { useAppDispatch } from "../store";
 import { deleteProfile } from "../store/householdSlice";
@@ -13,25 +13,55 @@ export default function ProfileScreen() {
     navigation.navigate("PickHousehold");
   };
 
+  // const handelActiveHousehold = () => {
+  //   dispatch(setActiveStatus({profileId, isActive}));
+  //   navigation.navigate("PickHousehold");
+  // };
+
   return (
-    <Button
-      style={[styles.button, { backgroundColor: "red" }]}
-      labelStyle={styles.buttonText}
-      onPress={handleLeaveHousehold}
-    >
-      Delete
-    </Button>
+    <View style={styles.container}>
+      <Button
+        style={[styles.button, { backgroundColor: "red" }]}
+        labelStyle={styles.buttonText}
+        onPress={handleLeaveHousehold}
+      >
+        Delete
+      </Button>
+      <View style={styles.spacing} />
+      <Button
+        style={[styles.button, { backgroundColor: "green" }]}
+        labelStyle={styles.buttonText}
+        // onPress={() => handelActiveHousehold(profileId, true)}
+      >
+        Active
+      </Button>
+      <View style={styles.spacing} />
+      <Button
+        style={styles.button}
+        labelStyle={styles.buttonText}
+        onPress={handleLeaveHousehold}
+      >
+        Admin
+      </Button>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
   button: {
     paddingVertical: 10,
-    backgroundColor: "white",
+    backgroundColor: "black",
     margin: 10,
   },
   buttonText: {
-    color: "black",
+    color: "white",
     fontSize: 20,
+  },
+  spacing: {
+    marginVertical: 15,
   },
 });
