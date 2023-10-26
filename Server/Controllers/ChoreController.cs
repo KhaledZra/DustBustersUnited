d50eea532fbd0864576374789d898eae4cc3e8d2
Khaled Zra <halo_434000@hotmail.com>
2023-10-26 13:19:04 +0200

    All endpoints now print the result

--- Server/Controllers/ChoreController.cs
+++ Server/Controllers/ChoreController.cs
@@ -18,6 +18,7 @@ public class ChoreController : ControllerBase
     {
         var chores = _context.Chores.ToList();
 
+        Console.WriteLine($"Code: 200, Ok!");
         return Ok(chores);
     }
 
@@ -28,8 +29,10 @@ public class ChoreController : ControllerBase
             .FirstOrDefault(h => h.Id == id);
         if (chore == null)
         {
+            Console.WriteLine($"Code: 404, Chore not found!");
             return NotFound();
         }
+        Console.WriteLine($"Code: 200, Ok!");
         return Ok(chore);
     }
 
@@ -39,6 +42,7 @@ public class ChoreController : ControllerBase
         var isFound = await HouseholdController.ConfirmHouseholdId(choreDto.HouseholdId, _context);
         if (isFound == false)
         {
+            Console.WriteLine($"Code: 422, Household does not exist!");
             return UnprocessableEntity("Bad householdId");
         }
         
@@ -46,6 +50,7 @@ public class ChoreController : ControllerBase
         _context.Chores.Add(chore);
         await _context.SaveChangesAsync();
         
+        Console.WriteLine($"Code: 201, Chore created!");
         return CreatedAtAction("GetChore", new { id = chore.Id }, chore);
     }
     
@@ -54,10 +59,15 @@ public class ChoreController : ControllerBase
     {
         var chore = _context.Chores.FirstOrDefault(chore => chore.Id == choreId);
 
-        if (chore == null) return NotFound("Chore not found");
+        if (chore == null)
+        {
+            Console.WriteLine($"Code: 404, Chore was not found!");
+            return NotFound("Chore not found");
+        }
 
         chore.IsActive = !chore.IsActive;
         _context.SaveChanges();
+        Console.WriteLine($"Code: 202, Chore IsActive set to: {chore.IsActive}!");
         return AcceptedAtAction("GetChore", new { id = chore.Id }, chore);
     }
     
@@ -66,10 +76,15 @@ public class ChoreController : ControllerBase
     {
         var chore = _context.Chores.FirstOrDefault(chore => chore.Id == choreId);
 
-        if (chore == null) return NotFound("Chore not found");
+        if (chore == null)
+        {
+            Console.WriteLine("Code: 404, Chore was not found!");
+            return NotFound("Chore not found");
+        }
 
         _context.Chores.Remove(chore);
         _context.SaveChanges();
+        Console.WriteLine("Code: 200, Chore was deleted!");
         return Ok($"ChoreId: {choreId}, was deleted");
     }
 
