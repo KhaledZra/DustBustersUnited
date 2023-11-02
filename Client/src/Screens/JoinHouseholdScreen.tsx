ab18aa51664e7f4120bcc910f34dc0b821d93011
Omar <omar.m10@hotmail.com>
2023-11-02 15:41:21 +0100

    Messi ballon Dior 2023.... BOOOM

--- Client/src/Screens/JoinHouseholdScreen.tsx
+++ Client/src/Screens/JoinHouseholdScreen.tsx
@@ -11,7 +11,7 @@ import {
 import { JoinHouseholdDto } from "../Data/Household";
 import { joinHousehold } from "../store/userSlice/thunks";
 import s from "../utils/globalStyles";
-import { RouteProp, useFocusEffect, useRoute } from "@react-navigation/native";
+import { RouteProp, useRoute } from "@react-navigation/native";
 import { Controller, useForm } from "react-hook-form";
 import { RootStackParamList } from "../Navigators/RootStackNavigator";
 import { RootStackScreenProps } from "../../types";
